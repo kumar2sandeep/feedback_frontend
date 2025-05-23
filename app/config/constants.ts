55720fc846573bf02a36a394699c3e7348f62405
kumar2sandeep <sandeep.kumar2@telusinternational.com>
2025-05-23 19:42:34 +0530

    url changes

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://feedback-backend-nwu3.onrender.com/api'; 
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://form-demo-backend.onrender.com'; 
